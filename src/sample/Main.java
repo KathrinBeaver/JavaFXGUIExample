7df306bd2282a016b9a08ef11c4b2129168ea558
peitos <andryhoy@gmail.com>
2022-04-02 00:23:24 +0300

    убрал комментарии с кодом и лишний файлик

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,18 +14,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("GUIMainForm");
 
-//        Label helloWorldLabel = new Label("Label on custom scene!");
-//        helloWorldLabel.setAlignment(Pos.CENTER);
-//        Scene primaryScene = new Scene(helloWorldLabel);
-//        primaryStage.setScene(primaryScene);
-
-//        Group root = new FiguresGroup();
-        Group root = new ElementsGroup();
-//        Pane root = new ElementsGridPane();
-//        Pane root = new ElementsFlowPane();
+        Label helloWorldLabel = new Label("Label on custom scene!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Group root = new ElementsFlowPane();
         primaryStage.setScene(new Scene(root, 300, 275));
 
         primaryStage.show();
