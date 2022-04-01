d9af2d2f2a6706e30023e07af71391f3fc15dd67
artyomchumachenko <artyomchumachenko@mail.ru>
2022-04-02 00:23:07 +0300

    artyom review has been ready

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import sample.components.ElementsFlowPane;
@@ -10,23 +14,25 @@ import sample.components.ElementsGridPane;
 import sample.components.ElementsGroup;
 import sample.components.FiguresGroup;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("GUIMainForm");
 
-//        Label helloWorldLabel = new Label("Label on custom scene!");
-//        helloWorldLabel.setAlignment(Pos.CENTER);
-//        Scene primaryScene = new Scene(helloWorldLabel);
-//        primaryStage.setScene(primaryScene);
+        Label helloWorldLabel = new Label("Label on custom scene!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Scene primaryScene = new Scene(helloWorldLabel);
+        primaryStage.setScene(primaryScene);
 
-//        Group root = new FiguresGroup();
-        Group root = new ElementsGroup();
-//        Pane root = new ElementsGridPane();
-//        Pane root = new ElementsFlowPane();
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root0 = new FiguresGroup();
+        Group root1 = new ElementsGroup();
+        Pane root2 = new ElementsGridPane();
+        Pane root3 = new ElementsFlowPane();
+        primaryStage.setScene(new Scene(root2, 300, 275));
 
         primaryStage.show();
     }
